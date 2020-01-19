21cd7bf17fc42857f3c552169eca1c7fb629eca9
anna-xing <anna.xing.22@gmail.com>
2020-01-19 09:05:49 -0500

    integrating js and html

--- addscript.js
+++ addscript.js
@@ -1,47 +1,53 @@
-// Recording restroom or FHP or both
+window.onload = function () {
 
-let addRestroom = false;
-let addFHP = false;
+    // Recording restroom or FHP or both
 
-let nodeType = Document.querySelectorAll(".node-btn");
+    let addRestroom = false;
+    let addFHP = false;
 
-for (let i = 0; i < nodeType.length; i++) {
-    nodeType[i].addEventListener("click", function () {
-        nodeType[i].classList.toggle("active");
+    let nodeType = Array.from(document.querySelectorAll(".node-btn"));
+
+    for (let i = 0; i < nodeType.length; i++) {
+        nodeType[i].addEventListener("click", function () {
+            nodeType[i].classList.toggle("active");
+            nodeType[i].classList.toggle("btn-primary");
+            nodeType[i].classList.toggle("btn-outline-primary");
+        });
+    }
+
+    if (nodeType[0].classList.contains("active")) {
+        addRestroom = true;
+    }
+
+    if (nodeType[1].classList.contains("active")) {
+        addFHP = true;
+    }
+
+    // Recording address input
+
+    let submitBtn = document.querySelector("#submit");
+    let addressField = document.querySelector("#address");
+    let userPosnField = document.querySelector("#user-location");
+    let address;
+
+    addressField.addEventListener("click", function () {
+        tempAddress = addressField.textContent;
+        address = tempAddress + 0// !!! TODO: CALL API HERE TO INTERPRET LAT+LONG OF ADDRESS
     });
-} 
 
-if (nodeType[0].classList.contains("active")) {
-    addRestroom = true;
+    userPosnField.addEventListener("click", function () {
+        address = 0;// !!! TODO: CALL API HERE TO DETERMINE USER LOCATION (LAT+LONG)
+    });
+
+    function Location() {
+        this.latitude = address + 0; //!!!!!get latitude of address
+        this.longitude = address + 0; //!!get long of address
+    }
+
+    submitBtn.addEventListener("click", function () {
+        // window[]
+    });
+
+
+
 }
-
-if (nodeType[1].classList.contains("active")) {
-    addFHP = true;
-}
-
-// Recording address input
-
-let submitBtn = Document.querySelector("#submit");
-let addressField = Document.querySelector("#address");
-let userPosnField = Document.querySelector("#user-location");
-let address;
-
-addressField.addEventListener("click", function () {
-    address = addressField.textContent;
-    address = tempAddress + 0// !!! TODO: CALL API HERE TO INTERPRET LAT+LONG OF ADDRESS
-});
-
-userPosnField.addEventListener("click", function () {
-    address = 0;// !!! TODO: CALL API HERE TO DETERMINE USER LOCATION (LAT+LONG)
-});
-
-function Location() {
-    this.latitude = address+0; //!!!!!get latitude of address
-    this.longitude = address+0; //!!get long of address
-}
-
-submitBtn.addEventListener("click", function () {
-    // window[]
-});
-
-
