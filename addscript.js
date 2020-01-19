4851ad54d8aa4d76f378c2826e7baef226ee9f05
anna-xing <anna.xing.22@gmail.com>
2020-01-19 12:55:38 -0500

    final?

--- addscript.js
+++ addscript.js
@@ -30,33 +30,36 @@ window.onload = function() {
     let userPosnField = document.querySelector("#user-location");
     let address;
 
-    addressField.addEventListener("click", function() {
-        tempAddress = addressField.textContent;
-        address = tempAddress + 0; // call api to interpret input as valid address
-    });
+    // addressField.addEventListener("click", function() {
+    //     tempAddress = addressField.textContent;
+    //     address = tempAddress + 0; // call api to interpret input as valid address
+    // });
 
-    userPosnField.addEventListener("click", function() {
-        address = 0; // call api to determine user address
-    });
+    // function getLatitude(place) {
+    //     //insert api here for getting latitude from address
+    // }
 
-    function getLatitude(place) {
-        //insert api here for getting latitude from address
-    }
+    // function getLongitude(place) {
+    //     // insert api here for getting longitude from address
+    // }
 
-    function getLongitude(place) {
-        // insert api here for getting longitude from address
-    }
+    // function Location(x) {
+    //     this.latitude = getLatitude(x);
+    //     this.longitude = getLongitude(x);
+    // }
 
-    function Location(x) {
-        this.latitude = getLatitude(x);
-        this.longitude = getLongitude(x);
-    }
-
-    let locationList = [];
+    // let locationList = [];
+    // // Science Teaching Complex
+    // locationList.push(new google.maps.LatLng(43.470536, -80.543526));
+    // // Earth Sciences Museum
+    // locationList.push(new google.maps.LatLng(43.471549, -80.541832));
+    // // QNC
+    // locationList.push(new google.maps.LatLng(43.471319, -80.543943));
 
     submitBtn.addEventListener("click", function() {
-        newLocation = Location(address);
+        newLocation = new Location(address);
         locationList.push(newLocation);
+        window.location.href = "searchmap.html";
     });
 
 }
