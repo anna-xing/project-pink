6b17b6708792151478b3f083eec07c55594a850c
anna-xing <anna.xing.22@gmail.com>
2020-01-19 11:14:01 -0500

    need to add api calls in addscript.js

--- addscript.js
+++ addscript.js
@@ -1,4 +1,4 @@
-window.onload = function () {
+window.onload = function() {
 
     // Recording restroom or FHP or both
 
@@ -8,10 +8,10 @@ window.onload = function () {
     let nodeType = Array.from(document.querySelectorAll(".node-btn"));
 
     for (let i = 0; i < nodeType.length; i++) {
-        nodeType[i].addEventListener("click", function () {
+        nodeType[i].addEventListener("click", function() {
             nodeType[i].classList.toggle("active");
-            nodeType[i].classList.toggle("btn-primary");
-            nodeType[i].classList.toggle("btn-outline-primary");
+            nodeType[i].classList.toggle("btn-outline-dark");
+            nodeType[i].classList.toggle("btn-dark");
         });
     }
 
@@ -30,24 +30,33 @@ window.onload = function () {
     let userPosnField = document.querySelector("#user-location");
     let address;
 
-    addressField.addEventListener("click", function () {
+    addressField.addEventListener("click", function() {
         tempAddress = addressField.textContent;
-        address = tempAddress + 0// !!! TODO: CALL API HERE TO INTERPRET LAT+LONG OF ADDRESS
+        address = tempAddress + 0; // call api to interpret input as valid address
     });
 
-    userPosnField.addEventListener("click", function () {
-        address = 0;// !!! TODO: CALL API HERE TO DETERMINE USER LOCATION (LAT+LONG)
+    userPosnField.addEventListener("click", function() {
+        address = 0; // call api to determine user address
     });
 
-    function Location() {
-        this.latitude = address + 0; //!!!!!get latitude of address
-        this.longitude = address + 0; //!!get long of address
+    function getLatitude(place) {
+        //insert api here for getting latitude from address
     }
 
-    submitBtn.addEventListener("click", function () {
-        // window[]
+    function getLongitude(place) {
+        // insert api here for getting longitude from address
+    }
+
+    function Location(x) {
+        this.latitude = getLatitude(x);
+        this.longitude = getLongitude(x);
+    }
+
+    let locationList = [];
+
+    submitBtn.addEventListener("click", function() {
+        newLocation = Location(address);
+        locationList.push(newLocation);
     });
-
-
 
 }
